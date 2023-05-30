31fbc15598e3fed5c9ada06fb7b4769a1cc302cd
Alberto-Vgs <alberto_b03@outlook.com>
2023-05-30 06:27:53 +0100

    Se sube codigo de Madlib

---
+++ Basics/Madlibs/madlibs.py
@@ -0,0 +1,24 @@
+def madlib(place1, name, name1, enemies, holiday):
+    print(f"""
+        Érase una vez en un pequeño pueblo de {place1} llamado {name}. Allí vivía un joven llamado {name1}, apasionado por la tecnología y conocido por todos como '{name1} Hacker'. Aunque sus habilidades informáticas eran impresionantes, {name1} soñaba con ser más que solo un hacker, quería convertirse en el héroe de su país.
+
+        Un día, mientras navegaba por la red, {name1} descubrió un complot malvado que amenazaba la paz de {place1}. Un grupo de {enemies} cibernéticos había infiltrado los sistemas del gobierno y planeaban sabotear las celebraciones del {holiday}. Determinado a frustrar sus planes y convertirse en el héroe de su país, {name1} decidió actuar.
+
+        Armado con su confiable computadora portátil, {name1} comenzó a trazar un plan. Utilizando sus habilidades de hacking, infiltró los servidores del grupo de {enemies} y comenzó a recopilar información sobre sus planes. Descubrió que planeaban lanzar un virus que interrumpiría todas las comunicaciones durante el desfile del {holiday}.
+
+        {name1} sabía que tenía que actuar rápidamente. Decidió convocar a un equipo de expertos en tecnología de su pueblo para ayudarlo en su misión. Reunió a sus amigos Pedro, un genio de la programación, y María, una experta en sistemas de seguridad.
+
+        Juntos, el equipo de {name1} diseñó un plan ingenioso. Utilizarían sus habilidades para rastrear a los {enemies}, desactivar el virus y proteger el desfile. Prepararon sus computadoras portátiles, equipadas con programas antivirus mejorados y herramientas de hacking personalizadas.
+
+        Llegó el gran día del desfile y {name1} y su equipo estaban listos para actuar. Se posicionaron estratégicamente en diferentes puntos del evento, cada uno monitoreando las comunicaciones y los sistemas de seguridad.
+
+        Cuando los {enemies} intentaron lanzar el virus, {name1} y su equipo estuvieron un paso adelante. Desactivaron el malware justo a tiempo, evitando un caos total. Las comunicaciones volvieron a la normalidad y el desfile continuó sin problemas.
+
+        La multitud estalló en aplausos y vítores cuando se enteraron de lo que {name1} y su equipo habían logrado. {name1} se convirtió en el héroe del pueblo y su historia se extendió rápidamente por todo {place1}.
+
+        A partir de ese día, {name1} fue reconocido como el protector de la tecnología y la seguridad en {name}. Recibió numerosas ofertas de trabajo en importantes empresas de ciberseguridad y se convirtió en un referente para los jóvenes interesados en la informática.
+
+        Pero, a pesar de su fama y éxito, {name1} siempre recordaba sus raíces y la importancia de su comunidad. Utilizó su talento para ayudar a proteger a {place1} de amenazas cibernéticas y se convirtió en un verdadero héroe para su país.
+
+        Y así, el hacker {name1} se convirtió en el héroe de {place1}, demostrando que, con ingenio y determinación, incluso un hacker puede usar sus habilidades para el bien de su pueblo.
+    """)
---
+++ Basics/Madlibs/start_madlib.py
@@ -0,0 +1,9 @@
+from madlibs import madlib
+if __name__ == "__main__":
+    place1 = input("Place: ")
+    name = input("Name: ")
+    name1 = input("Name: ")
+    enemies = input("Enemies: ")
+    holiday = input("Holiday: ")
+
+    madlib(place1, name, name1, enemies, holiday)
